d389bce06dda4a5c1d0e9ca4cec33b90dbff7487
agisnarevaldo <agisrifaldo75@gmail.com>
2022-04-10 23:42:07 +0700

    membuat dan menambahkan kode Perulangan.java

---
+++ src/com/belajar/Perulangan.java
@@ -0,0 +1,10 @@
+package com.belajar;
+
+public class Perulangan {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println("Hitungan Ke - " + i);
+        }
+    }
+    
+}
